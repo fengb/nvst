8c760df4d63b9462f92a113e648caaf674685c5d
fengb <contact@fengb.info>
2013-12-27 20:38:31 -0600

    Minor refactor

--- app/models/investment_price.rb
+++ app/models/investment_price.rb
@@ -3,5 +3,5 @@ class InvestmentPrice < ActiveRecord::Base
   has_one    :dividend,   class_name: 'InvestmentDividend'
   has_one    :split,      class_name: 'InvestmentSplit'
 
-  scope :year_range, ->(end_date=Date.today) { where('date >= ? AND date <= ?', end_date - 365, end_date) }
+  scope :year_range, ->(end_date=Date.today) { where(date: (end_date - 365)..end_date) }
 end
