5c9837239bd5038c4de0264e464517c38f1fa8f7
fengb <contact@fengb.info>
2013-12-28 00:57:02 -0600

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+path = File.dirname(__FILE__)
+connection = ActiveRecord::Base.connection
+statements = IO.read(path + '/seeds.sql').split(';').map(&:strip).reject(&:empty?)
+
+ActiveRecord::Base.transaction do
+  statements.each do |statement|
+    connection.execute(statement)
+  end
+end
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO investments(symbol, name,          auto_update)
+                 VALUES('USD', 'U.S. Dollars', false),
+                       ('KO',  'Coca-Cola',    true),
+                       ('AAPL','Apple Inc.',   true);
