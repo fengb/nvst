fd062b9b0684543082a0fe0943adc3ee63c72937
fengb <contact@fengb.info>
2013-12-28 11:22:46 -0600

    Why use inner class when outer class body is unused?

--- app/jobs/populate_investment_job.rb
+++ app/jobs/populate_investment_job.rb
@@ -10,59 +10,53 @@ class PopulateInvestmentJob
     end
 
     def perform(investment_id)
-      Populate.run!(Investment.find(investment_id))
+      self.new(Investment.find(investment_id)).run!
     end
   end
 
-  class Populate
-    def initialize(investment)
-      @investment = investment
-      @latest_date = InvestmentPrice.where(investment: @investment).order('date DESC').pluck(:date).first || Date.new
+  def initialize(investment)
+    @investment = investment
+    @latest_date = InvestmentPrice.where(investment: @investment).order('date DESC').pluck(:date).first || Date.new
+  end
+
+  def run!
+    ActiveRecord::Base.transaction do
+      prices!
+      dividends!
+      splits!
     end
+  end
 
-    def self.run!(investment)
-      Populate.new(investment).run!
+  def prices!
+    YahooFinance.historical_prices(@investment.symbol).each do |row|
+      return if row.date <= @latest_date
+
+      InvestmentPrice.create!(investment: @investment,
+                              date:       row.date,
+                              high:       row.high,
+                              low:        row.low,
+                              close:      row.close)
     end
+  end
 
-    def run!
-      ActiveRecord::Base.transaction do
-        prices!
-        dividends!
-        splits!
-      end
+  def dividends!
+    YahooFinance.dividends(@investment.symbol).each do |row|
+      return if row.date <= @latest_date
+
+      InvestmentDividend.create!(investment: @investment,
+                                 date:       row.date,
+                                 amount:     row.amount)
     end
+  end
 
-    def prices!
-      YahooFinance.historical_prices(@investment.symbol).each do |row|
-        return if row.date <= @latest_date
+  def splits!
+    YahooFinance.splits(@investment.symbol).each do |row|
+      return if row.date <= @latest_date
 
-        InvestmentPrice.create!(investment: @investment,
-                                date:       row.date,
-                                high:       row.high,
-                                low:        row.low,
-                                close:      row.close)
-      end
-    end
-
-    def dividends!
-      YahooFinance.dividends(@investment.symbol).each do |row|
-        return if row.date <= @latest_date
-
-        InvestmentDividend.create!(investment: @investment,
-                                   date:       row.date,
-                                   amount:     row.amount)
-      end
-    end
-
-    def splits!
-      YahooFinance.splits(@investment.symbol).each do |row|
-        return if row.date <= @latest_date
-
-        InvestmentSplit.create!(investment: @investment,
-                                date:       row.date,
-                                before:     row.before,
-                                after:      row.after)
-      end
+      InvestmentSplit.create!(investment: @investment,
+                              date:       row.date,
+                              before:     row.before,
+                              after:      row.after)
     end
   end
 end
