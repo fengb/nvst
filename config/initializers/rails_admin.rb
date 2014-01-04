34558e85172fcd81ee863a9e0456599d0e81844e
fengb <contact@fengb.info>
2014-01-04 17:43:39 -0600

    Added Transfer to RailsAdmin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -26,7 +26,7 @@ RailsAdmin.config do |config|
   # config.default_items_per_page = 20
 
   # config.excluded_models = ['Admin', 'Investment', 'InvestmentDividend', 'InvestmentPrice', 'InvestmentSplit', 'User']
-  config.included_models = %w[Admin User Investment Contribution Trade Event Expense]
+  config.included_models = %w[Admin User Investment Contribution Transfer Trade Event Expense]
 
   # Label methods for model instances:
   config.label_methods = [:title, :name]
