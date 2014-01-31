57fcfca71b83e79cf1554d7a04a71f9928f32484
fengb <contact@fengb.info>
2014-01-31 14:31:39 -0600

    Slim down presenters

--- app/presenters/tax_docs/form1065_presenter.rb
+++ app/presenters/tax_docs/form1065_presenter.rb
@@ -4,27 +4,6 @@ module TaxDocs
       @year = year
     end
 
-    def users
-      # FIXME: return active users
-      User.all
-    end
-
-    def interest
-      events['interest']
-    end
-
-    def ordinary_dividends
-      events['dividend - ordinary']
-    end
-
-    def qualified_dividends
-      events['dividend - qualified']
-    end
-
-    def tax_exempt_dividends
-      events['dividend - tax-exempt']
-    end
-
     def expense_categories
       Expense.category.values
     end
@@ -32,17 +11,6 @@ module TaxDocs
     def expenses(category=nil)
       @expenses ||= Expense.year(@year).to_a
       category.nil? ? @expenses : @expenses.select{|e| e.category == category}
-    end
-
-    def events
-      @events ||= begin
-        events = Event.year(@year)
-        Hash[Event.category.values.map{|c| [c, events.select{|e| e.category == c}]}]
-      end
-    end
-
-    def close_transactions
-      @close_transactions ||= Transaction.year(@year).tracked.close.order(:date).to_a
     end
   end
 end
