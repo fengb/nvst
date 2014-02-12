184e1c65d88f4448c4054d1966e5801a12c6561a
fengb <contact@fengb.info>
2014-02-12 09:39:27 -0600

    Get cashflow and principal

--- app/presenters/transactions_growth_presenter.rb
+++ app/presenters/transactions_growth_presenter.rb
@@ -1,18 +1,34 @@
 class TransactionsGrowthPresenter
   def self.all
-    self.new(Transaction.includes(lot: :investment))
+    self.new(transactions: Transaction.includes(lot: :investment),
+             cashflows:    Contribution.all + Expense.all)
   end
 
-  def initialize(transactions)
+  def initialize(transactions: [],
+                 cashflows:    [])
     @price_matchers = {}
     @shares_matchers = {}
     transactions.group_by(&:investment).each do |inv, transactions|
       @shares_matchers[inv] = BestMatchHash.sum(transactions.map{|t| [t.date, t.shares]})
     end
+    @principal_matcher = BestMatchHash.sum(cashflows.map{|c| [c.date, c.amount]})
+    @cashflow_amounts = {}
+    cashflows.each do |cashflow|
+      @cashflow_amounts[cashflow.date] ||= 0
+      @cashflow_amounts[cashflow.date] += cashflow.amount
+    end
   end
 
   def value_at(date)
     investments.sum{|i| value_for(i, date)}
+  end
+
+  def principal_at(date)
+    @principal_matcher[date]
+  end
+
+  def cashflow_at(date)
+    @cashflow_amounts[date] || 0
   end
 
   private
--- spec/presenters/transactions_growth_presenter_spec.rb
+++ spec/presenters/transactions_growth_presenter_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe TransactionsGrowthPresenter do
   describe '#value_at' do
     let(:presenter) do
-      TransactionsGrowthPresenter.new([
+      TransactionsGrowthPresenter.new(transactions: [
         double(investment: 'FOO', date: '2013-01-02', shares: 100),
         double(investment: 'FOO', date: '2013-01-03', shares: 100),
         double(investment: 'BAR', date: '2013-01-04', shares: 100)
@@ -29,6 +29,52 @@ describe TransactionsGrowthPresenter do
       presenter.should_receive(:price_for).with('FOO', '2013-01-04').and_return(2)
       presenter.should_receive(:price_for).with('BAR', '2013-01-04').and_return(1)
       expect(presenter.value_at('2013-01-04')).to eq(500)
+    end
+  end
+
+  context 'cashflows' do
+    subject do
+      TransactionsGrowthPresenter.new(cashflows: [
+        double(date: '2013-01-04', amount: 10),
+        double(date: '2013-01-05', amount: 100),
+        double(date: '2013-01-05', amount: 200)
+      ])
+    end
+
+    describe '#cashflow_at' do
+      it 'is 0 on 2013-01-03' do
+        expect(subject.cashflow_at('2013-01-03')).to eq(0)
+      end
+
+      it 'is 10 on 2013-01-04' do
+        expect(subject.cashflow_at('2013-01-04')).to eq(10)
+      end
+
+      it 'is 300 on 2013-01-05' do
+        expect(subject.cashflow_at('2013-01-05')).to eq(300)
+      end
+
+      it 'is 0 on 2013-01-06' do
+        expect(subject.cashflow_at('2013-01-06')).to eq(0)
+      end
+    end
+
+    describe '#principal_at' do
+      it 'is 0 on 2013-01-03' do
+        expect(subject.principal_at('2013-01-03')).to eq(0)
+      end
+
+      it 'is 10 on 2013-01-04' do
+        expect(subject.principal_at('2013-01-04')).to eq(10)
+      end
+
+      it 'is 110 on 2013-01-05' do
+        expect(subject.principal_at('2013-01-05')).to eq(310)
+      end
+
+      it 'is 110 on 2013-01-06' do
+        expect(subject.principal_at('2013-01-06')).to eq(310)
+      end
     end
   end
 end
