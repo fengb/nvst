e62884e6cacc6b33e3fa14f03642d079f0137327
fengb <contact@fengb.info>
2014-02-14 13:16:02 -0600

    Removed unused private method

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -2,9 +2,4 @@ class PortfolioController < ApplicationController
   def show
     @growth = PublicPortfolioPresenter.all(10000)
   end
-
-  private
-  def contributions
-    Contribution.where('user_id != ?', 1)
-  end
 end
