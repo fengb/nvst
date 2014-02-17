fd87f673f0b50535cd17f747eacdf07ab0f3c4bd
fengb <contact@fengb.info>
2014-02-16 20:54:34 -0600

    Converted to class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,10 @@ class User < ActiveRecord::Base
   has_many :contributions
   has_many :ownerships
 
-  scope :fee_collector, ->{find_by(is_fee_collector: true)}
   scope :fee_collecting, ->{where(is_fee_collector: false)}
+  def self.fee_collector
+    find_by(is_fee_collector: true)
+  end
 
   def title
     email
