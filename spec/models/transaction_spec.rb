c24bc5dac9482e27c4390b139f050bad43748b61
fengb <contact@fengb.info>
2014-05-23 17:39:59 -0500

    Removed stale test

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,19 +1,6 @@
 require 'spec_helper'
 
 describe Transaction do
-  describe '#open?' do
-    let!(:lot)          { FactoryGirl.create(:lot) }
-    let(:transaction1)  { lot.transactions.first }
-    let!(:transaction2) { FactoryGirl.create(:transaction, lot: lot,
-                                                           date: transaction1.date,
-                                                           price: transaction1.price) }
-
-    it 'is true when data matches lot' do
-      expect(transaction1.open?).to be(true)
-      expect(transaction2.open?).to be(true)
-    end
-  end
-
   describe '#adjusted_price' do
     subject { FactoryGirl.create(:transaction) }
 
