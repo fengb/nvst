8c53ab228deed19b3c317f99f7a07b9c179043ca
fengb <contact@fengb.info>
2014-08-04 08:10:03 -0500

    Updated seeding to reflect new design

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,6 @@
 INSERT INTO users(email,   encrypted_password, is_fee_collector)
            VALUES('admin', '',                 true);
 
-INSERT INTO investments(symbol, name,               category)
-                 VALUES('USD',  'U.S. Dollars',     'cash'),
-                       ('SPY',  'SPDR S&P 500 ETF', 'benchmark');
-
-INSERT INTO investment_historical_prices(investment_id,         date, high, low, close, adjustment)
-                                  SELECT id,            '1900-01-01',    1,   1,     1,          1
-                                    FROM investments
-                                   WHERE symbol='USD';
+INSERT INTO investments(symbol, name,               type)
+                 VALUES('USD',  'U.S. Dollars',     'Investment::Cash'),
+                       ('SPY',  'SPDR S&P 500 ETF', 'Investment::Stock');
