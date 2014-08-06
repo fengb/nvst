c09479d908d1b58f77676b6830c1133efaad94d4
fengb <contact@fengb.info>
2014-08-05 19:10:16 -0500

    Removed generate_activities! from factory.  Evil pattern!

--- spec/factories/contributions.rb
+++ spec/factories/contributions.rb
@@ -2,13 +2,5 @@ FactoryGirl.define do
   factory :contribution do
     date   { Date.today - rand(100) }
     amount { rand(10000.0..100000.0).to_d.round(2) }
-
-    ignore do
-      generate_activities true
-    end
-
-    after(:create) do |contribution, evaluator|
-      contribution.generate_activities! if evaluator.generate_activities
-    end
   end
 end
