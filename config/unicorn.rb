ec36bb65a85a2c4282719c934cd8c25a41111a90
fengb <contact@fengb.info>
2014-08-23 00:54:13 -0500

    Server side scripts

---
+++ config/unicorn.rb
@@ -0,0 +1,35 @@
+working_directory ENV['NVST_ROOT']
+stderr_path ENV['NVST_ROOT'] + "/log/unicorn.stderr.log"
+stdout_path ENV['NVST_ROOT'] + "/log/unicorn.stdout.log"
+pid ENV['NVST_ROOT'] + "/tmp/pids/unicorn.pid"
+
+worker_processes Integer(ENV["NVST_WORKERS"] || 3)
+timeout 30
+preload_app true
+
+before_fork do |server, worker|
+  # Kill obsolete server (USR2 signal)
+  old_pid = ENV['NVST_ROOT'] + "/tmp/pids/unicorn.pid.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
