d248a972f984aa3fe2b549364b54e238774a5f42
fengb <contact@fengb.info>
2014-12-27 20:09:04 -0700

    Clean up graphing

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -1,12 +1,13 @@
 Mutiny.widgets.graph = {
-  defaults: { width: 900, height: 500 },
   init: function(instigator, options){
     d3.json(options.url, function(error, data) {
       if (error) return console.warn(error);
 
-      var svg = dimple.newSvg(instigator, options.width, options.height)
+      var width = options.width || instigator.offsetWidth
+      var height = options.height || instigator.offsetHeight
+      var svg = dimple.newSvg(instigator, width, height)
       var chart = new dimple.chart(svg, data)
-      chart.setBounds(-1, 0, options.width + 1, options.height)
+      chart.setBounds(-1, 0, width + 1, height)
       chart.addCategoryAxis('x', options.xAxis)
       chart.addMeasureAxis('y', options.yAxis)
       chart.addSeries(options.series, dimple.plot.line)
