3a60d8339b10db18d7b094dc85b4ce79d0f041c5
fengb <contact@fengb.info>
2014-12-30 20:02:15 -0600

    Changed USD since it is a part of seeds

--- spec/models/investment_spec.rb
+++ spec/models/investment_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 RSpec.describe Investment, type: :model do
   describe '.lookup_by_symbol' do
     let!(:investments) do
-      [ FactoryGirl.create(:cash, symbol: 'USD'),
+      [ FactoryGirl.create(:cash, symbol: 'BTC'),
         FactoryGirl.create(:cash, symbol: 'CAD'),
       ]
     end
@@ -12,7 +12,7 @@ RSpec.describe Investment, type: :model do
     it 'uses symbols as keys' do
       lookup = Investment.lookup_by_symbol
       expect(lookup).to include(
-        'USD' => investments[0],
+        'BTC' => investments[0],
         'CAD' => investments[1],
       )
     end
@@ -21,7 +21,7 @@ RSpec.describe Investment, type: :model do
       investments[0].update!(past_symbols: ['AUD', 'GBP'])
       lookup = Investment.lookup_by_symbol
       expect(lookup).to include(
-        'USD' => investments[0],
+        'BTC' => investments[0],
         'AUD' => investments[0],
         'GBP' => investments[0],
         'CAD' => investments[1],
