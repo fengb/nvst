d3ce6d9e650c7b26c1f8fcaf41f6f917decfa0da
fengb <contact@fengb.info>
2014-12-30 19:46:42 -0600

    Clearly not copied from Bitvain

--- config/eye.rb
+++ config/eye.rb
@@ -2,4 +2,4 @@ Eye.load './eye/*.rb'
 
 config_file = File.expand_path('../../.eye', __FILE__)
 configs = IO.read(config_file).split
-Eye::Bitvain.start_with(*configs)
+Eye::Nvst.start_with(*configs)
