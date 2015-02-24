463b724ff14279cd579d3927c5d2be86f8e9f4d9
fengb <contact@fengb.info>
2015-02-23 21:06:43 -0600

    Restyled graph

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -2,6 +2,28 @@
 //= require chartist/dist/chartist.js
 
 Mutiny.widgets.graph = {
+  defaults: {
+    low: 0,
+    chartPadding: 0,
+    showPoint: false,
+    axisX: {
+      offset: 20,
+      labelInterpolationFnc: function(value, index, source){
+        var yyyy = value.substr(0, 4)
+        var prevValue = source[index - 1]
+
+        if(index === 0 || prevValue.indexOf(yyyy) !== 0){
+          return yyyy
+        }
+
+        return null
+      },
+    },
+    axisY: {
+      offset: 50,
+    }
+  },
+
   init: function(instigator, options){
     var data = options.data
     options.data = null
