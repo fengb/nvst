ae8e95a0d31fb7cabc76e334113afb85553e81de
fengb <contact@fengb.info>
2015-03-16 18:28:13 -0500

    Split DbBackup to smaller chunks

--- lib/job/db_backup.rb
+++ lib/job/db_backup.rb
@@ -8,38 +8,44 @@ module Job
     MESSAGE = ENV['NVST_DB_BACKUP_MSG']
 
     def self.perform
-      db = Nvst::Application.config.database_configuration['default']
+      db_dump!(Nvst::Application.config.database_configuration['default'] , TMP)
 
+      if TARGET.present?
+        FileUtils.mv TMP, TARGET
+      end
+
+      if MESSAGE
+        commit!(TARGET, MESSAGE)
+      end
+    end
+
+    def self.db_dump!(config, filename)
       system <<-CMD.gsub(/\s+/, ' ')
-        PGPASSWORD="#{db['password']}"
+        PGPASSWORD="#{config['password']}"
         pg_dump
           --data-only
           --no-owner
           --exclude-table=investment_*
           --exclude-table=schema_migrations
-          --username='#{db['username']}'
-          --host='#{db['host']}'
-          --port='#{db['port']}'
-          '#{db['database']}'
+          --username='#{config['username']}'
+          --host='#{config['host']}'
+          --port='#{config['port']}'
+          '#{config['database']}'
         |
         sed
           -e 's/^--.*//'
           -e '/^ *$/d'
-        > "#{TMP}"
+        > "#{filename}"
       CMD
+    end
 
-      if TARGET.present?
-        FileUtils.mv 'tmp/db-backup.sql', TARGET
-      end
+    def self.commit!(filename, message)
+      dirname, filename = File.split(filename)
 
-      if MESSAGE
-        dirname, filename = File.split(TARGET)
-
-        git = Git.open(dirname)
-        git.add(filename)
-        if %w[M A].include?(git.status[filename].type)
-          puts git.commit(MESSAGE)
-        end
+      git = Git.open(dirname)
+      git.add(filename)
+      if %w[M A].include?(git.status[filename].type)
+        puts git.commit(message)
       end
     end
   end
