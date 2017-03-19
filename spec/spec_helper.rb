428ac2e4be7807f4269e6703d29d91ffe69837d0
Benjamin Feng <contact@fengb.me>
2017-03-18 21:05:25 -0500

    Fix code climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,6 @@
 require 'simplecov'
 SimpleCov.start
 
-if ENV['CODECLIMATE_REPO_TOKEN']
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
-end
-
 RSpec.configure do |config|
   config.order = "random"
 end
