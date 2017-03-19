96ecc7b451deb8875790d2b3ed4d2961ea7c44bb
Benjamin Feng <contact@fengb.me>
2017-03-19 16:24:39 -0500

    "Simplify" RailsUtil.all

--- app/misc/rails_util.rb
+++ app/misc/rails_util.rb
@@ -1,5 +1,7 @@
 module RailsUtil
   def self.all(dir)
-    Dir[Rails.root + "app/#{dir}/*"].map{|f| File.basename f, '.rb'}.map{|m| m.camelize.constantize}
+    Dir[Rails.root + "app/#{dir}/*.rb"]
+      .map { |f| File.basename f, '.rb' }
+      .map { |m| m.camelize.constantize }
   end
 end
