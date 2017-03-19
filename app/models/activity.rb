5cb6955b175be0aec9030700b6d213dfab7aa305
Benjamin Feng <contact@fengb.me>
2017-03-18 20:40:53 -0500

    Helper to hunt down discrepancies

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -13,12 +13,21 @@ class Activity < ApplicationRecord
   delegate :investment, to: :position
 
   has_and_belongs_to_many :adjustments, class_name: 'ActivityAdjustment'
+  has_and_belongs_to_many :contributions
+  has_and_belongs_to_many :expenses
+  has_and_belongs_to_many :expirations
+  has_and_belongs_to_many :events
+  has_and_belongs_to_many :trades
 
   scope :tracked, ->{joins(position: :investment).where("investments.type != 'Investment::Cash'")}
   scope :opening, ->{where(is_opening: true)}
   scope :closing, ->{where(is_opening: false)}
   scope :buy,  ->{where('shares > 0')}
   scope :sell, ->{where('shares < 0')}
+
+  def source
+    [contributions + expenses + expiration + events + trades].first
+  end
 
   def value
     -shares * adjusted_price
